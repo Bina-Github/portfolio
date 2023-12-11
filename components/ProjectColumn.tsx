ac6003abb84c317f2777a57c483c88f940ca0648
Bina <BinaKleinerAls3@gmail.com>
2023-12-11 14:54:58 +0100

    ProjectColumn: changed Space between Boxes

--- components/ProjectColumn.tsx
+++ components/ProjectColumn.tsx
@@ -6,7 +6,7 @@ const ProjectColumn = () => {
   return (
     <div className="ml-[700px] mr-5 mt-20 w-full">
       {/* Headline */}
-      <div className="flex flex-row items-center justify-center mb-32">
+      <div className="flex flex-row items-center justify-center mb-24">
         <div className="h-0.5 grow bg-gradient-to-r from-cPurple to-cYellow"></div>
         <h1 className="text-cYellow mx-3 text-3xl">Ausbildung & Projekte</h1>
         <div className="h-0.5 grow bg-gradient-to-l from-cPurple to-cYellow"></div>
