436ae75a04da5edc7749ce7e2d94aace691b509e
Bina <BinaKleinerAls3@gmail.com>
2023-12-12 09:56:23 +0100

    page: paddingBottom for projectCol + overflowX hidden

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import ProjectColumn from "@/components/ProjectColumn";
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen">
-      <div className="relative mx-auto max-w-[2000px] flex flex-row ">
+    <main className="relative min-h-screen overflow-x-hidden">
+      <div className="relative mx-auto pb-8 max-w-[2000px] flex flex-row ">
         <DetailsColumn />
         <ProjectColumn />
       </div>
